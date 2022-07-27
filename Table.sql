1abff0f42558f8d438bc1a4a7326b38f7e60a404
Anil4466 <107477630+Anil4466@users.noreply.github.com>
2022-07-27 16:24:00 +0530

    Add files via upload

--- Table.sql
+++ Table.sql
@@ -1,4 +1,8 @@
 --create table employe(eid int,ename varchar(12),edept varchar(12),eaddress varchar(15));
 --insert into employe
 --values(123,"Anil","Manager","Ballari"),(134,"akhil","Account","Blore"),(112,"kumar","casher","Ballari"),(445,"pavan","Account","Ballari");
-select * from employe;
+--select * from employe;
+--select ename from employe;
+--update employe set eaddress="hydrabad" where ename="pavan";
+--select * from employe;
+ select * from employe where ename="Anil";
