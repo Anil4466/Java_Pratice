0ffcdabce3e9edad7725a2dbb41805b4cb0696e5
Anil4466 <107477630+Anil4466@users.noreply.github.com>
2022-07-27 16:14:11 +0530

    Add files via upload

---
+++ Table.sql
@@ -0,0 +1,4 @@
+--create table employe(eid int,ename varchar(12),edept varchar(12),eaddress varchar(15));
+--insert into employe
+--values(123,"Anil","Manager","Ballari"),(134,"akhil","Account","Blore"),(112,"kumar","casher","Ballari"),(445,"pavan","Account","Ballari");
+select * from employe;
